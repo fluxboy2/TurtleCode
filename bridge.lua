ce2c33eddf7121a2d67690e11f37418c7b826835
Thomas G <tommygarrison94@gmail.com>
2021-01-25 16:09:16 -0500

    changed to not turtle.placeDown()

--- bridge.lua
+++ bridge.lua
@@ -2,7 +2,7 @@ turtle.select(2)
 
 while turtle.getItemCount(2) ~= 0 and turtle.getFuelLevel() ~= 0 do
 
-    if turtle.placeDown() ~= true then
+    if not turtle.placeDown() then
         print("Can't place the block")
     end
 end
