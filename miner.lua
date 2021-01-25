e8d6d642967f03287412019bafcdd8b2bb0d824b
Thomas G <tommygarrison94@gmail.com>
2021-01-25 17:13:54 -0500

    changed to not turtle.placeDown()

--- miner.lua
+++ miner.lua
@@ -1,12 +1,10 @@
 local keep_bot_going = true
 fuel = 0
 
-function find
-
 function refuel_bot()
-    -- if turtle.getFuelLevel() <= 0 then
+    if turtle.getFuelLevel() <= 0 then
 
-    -- end
+    end
     for i=1, turtle.getItemDetail(1) do
         i = i + 1
     end
