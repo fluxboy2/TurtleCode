dbd7a1d3f53561912250246a249438a2074c2573
Thomas G <tommygarrison94@gmail.com>
2021-01-25 16:52:56 -0500

    changed to not turtle.placeDown()

--- bridge.lua
+++ bridge.lua
@@ -1,10 +1,16 @@
-local keep_bot_going = true
+turtle.select(2)
 
-function ihc_refuel()
-    print("test")
-end
--- turtle loop
+local keep_building = true
 
-while keep_bot_going do
-    ihc_refuel()
+while turtle.getItemCount(2) ~= 0 and turtle.getFuelLevel() ~= 0 and keep_building do
+
+    if not turtle.placeDown() then
+        print("Can't place the block")
+        if not turtle.forward() then
+            keep_building = false
+            print("Done")
+        end
+    else
+        print("Block placed")
+    end
 end
---
+++ save-readonly.lua
@@ -0,0 +1,10 @@
+local keep_bot_going = true
+
+function ihc_refuel()
+    print("test")
+end
+-- turtle loop
+
+while keep_bot_going do
+    ihc_refuel()
+end
